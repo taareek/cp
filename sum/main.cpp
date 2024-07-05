1d2868fa9cb17429b9b672b8935544ca1928a403
Tarek Aziz <tarekiub17@gmail.com>
2024-07-06 03:00:09 +0600

    changed

--- sum/main.cpp
+++ sum/main.cpp
@@ -4,12 +4,15 @@ using namespace std;
 
 int main()
 {
-    int a,b,c;
-    cin>>a>>b>>c;
-    if (a+b==c || b+c==a || a+c==b){
-       cout<<"YES";
-    }else{
-        cout<<"NO";
+    int a,b,c,t;
+    cin>>t;
+    for(int i=0; i<t; i++){
+        cin>>a>>b>>c;
+        if (a+b==c || b+c==a || a+c==b){
+           cout<<"YES\n";
+        }else{
+            cout<<"NO\n";
+        }
     }
 
     return 0;
